eccc8fe569b08e2de66e7fc15bed05886e48cf41
ctburley <calin.burley@gmail.com>
2018-04-24 09:58:12 -0400

    updated to run through jot_core

--- jot.py
+++ jot.py
@@ -6,10 +6,15 @@ class Plugin:
 
 	def __init__(self, bot):
 		self.bot = bot
-		self.patterns = (
+		self.patterns = {
 			'add':	re.compile('^;(?P<key>[\w\s]+?)\s+=\s+(?P<data>.*)$'),
-			#'get':	'',
-		)
+			'get':	re.compile('^;(?P<key>[\w\s]+?)\s*$'),
+			'tell':	re.compile('^;(?P<key>[\w\s]+?)\s*@\s*(?P<at>\S+)\s*$'),
+			'search':	re.compile('^;\?(?P<key>[\w\s]+?)\s*$'),
+			'remove':	re.compile('^;-(?P<key>[\w\s]+?)\s*$')
+		}
+		self.reserved = [ 'help' ]
+		
 		print("JOT ~ LOADED")
 			
 	@classmethod
@@ -17,68 +22,63 @@ class Plugin:
 		return cls(old.bot)
 	
 
-	@irc3.event(rfc.PRIVMSG)
-	def jot_core(self, mask, event, target, data, **kw):
+	@irc3.event('^(@\S+ )?:(?P<nick>\S+)!\S+@\S+ PRIVMSG (?P<target>\S+) :(?P<data>.*)$')
+	def jot_core(self, nick, target, data, **kw):
 		if (self.bot.obeying_commands(target)):
+			### Add
 			result = self.patterns['add'].match(data)
 			if result:
-				self.jot_plus(mask.nick, target, 
-				
+				self.jot_plus(nick, target, result.group('key'), result.group('data'))
+			### Append
+			### Get
+			result = self.patterns['get'].match(data)
+			if result:
+				self.jot_get(nick, target, result.group('key'))
+			### Tell
+			result = self.patterns['tell'].match(data)
+			if result:
+				self.jot_get(result.group('at'), target, result.group('key'))
+			### Search
+			result = self.patterns['search'].match(data)
+			if result:
+				self.jot_search(nick, target, result.group('key'))
+			### Delete
+			result = self.patterns['remove'].match(data)
+			if result:
+				self.jot_remove(nick, target, result.group('key'))
 	
 	def jot_plus(self, nick, target, key, data):
-			with shelve.open('jot.shelf') as jot:
-				if (key.lower() not in jot):
-					print("JOT~~~ +[" + nick + "] " + key + " " + data)
-					jot[key.lower()] = {'key':key, 'from':nick, 'value':data}
-					self.bot.privmsg(nick, 'Ok.')
-				else:
-					self.bot.privmsg(nick, "The key '"+key+"' already exists.")
+		with shelve.open('jot.shelf') as jot:
+			if (key.lower() not in jot):
+				jot[key.lower()] = {'key':key, 'from':nick, 'verb':'=', 'value':data}
+				self.bot.privmsg(nick, 'Ok.')
+			else:
+				self.bot.privmsg(nick, "The key '"+key+"' already exists.")
 			
-	@irc3.event('^(@(?P<tags>\S+) )?:(?P<nick>\S+)(?P<mask>!\S+@\S+) PRIVMSG (?P<target>\S+) :;(?P<key>[\w\s]+?)\s*$')
-	def jot_get(self, nick=None, mask=None, target=None, key=None, **kw):
-		if (self.bot.obeying_commands(target)):
-			with shelve.open('jot.shelf') as jot:
-				key = key.lower()
-				if (key in jot):
-					if 'verb' in jot[key]:
-						print("JOT~~~ [" + nick + "] " + jot[key]['key'] + " " + jot[key]['verb'] + " " + jot[key]['value'])
-						if (jot[key]['verb'] == '='):
-							self.bot.privmsg(target, nick + ": " + jot[key]['value'])
-						else:
-							return self.bot.privmsg(target, nick + ": " + jot[key]['key'] + " " + jot[key]['verb'] + " " + jot[key]['value'])
-	
-	@irc3.event('^(@(?P<tags>\S+) )?:(?P<nick>\S+)(?P<mask>!\S+@\S+) PRIVMSG (?P<target>\S+) :;(?P<key>[\w\s]+?)\s*@\s*(?P<audience>[^,]+)\s*$')
-	def jot_tell(self, nick=None, mask=None, target=None, audience=None, key=None, **kw):
-		if (self.bot.obeying_commands(target)):
-			with shelve.open('jot.shelf') as jot:
-				key = key.lower()
-				if (key in jot):
-					print("JOT~~~ [" + nick + "] " + jot[key]['key'] + " " + jot[key]['verb'] + " " + jot[key]['value'])
-					if (jot[key.lower()]['verb'] == '='):
-						self.bot.privmsg(target, audience.strip() + ": " + jot[key]['value'])
+	def jot_get(self, nick, target, key):
+		with shelve.open('jot.shelf') as jot:
+			key = key.lower()
+			if (key in jot):
+					if (jot[key]['verb'] == '='):
+						self.bot.privmsg(target, nick + ": " + jot[key]['value'])
 					else:
-						return self.bot.privmsg(target, audience + ": " + jot[key]['key'] + " " + jot[key]['verb'] + " " + jot[key]['value'])
+						return self.bot.privmsg(target, nick + ": " + jot[key]['key'] + " " + jot[key]['verb'] + " " + jot[key]['value'])
 	
-	@irc3.event('^(@(?P<tags>\S+) )?:(?P<nick>\S+)(?P<mask>!\S+@\S+) PRIVMSG (?P<target>\S+) :;\?(?P<key>[\w\s]+?)\s*$')
-	def jot_search(self, nick=None, mask=None, target=None, key=None, **kw):
-		if (self.bot.obeying_commands(target)):
-			with shelve.open('jot.shelf') as jot:
-				print ("JOTSEARCH~~~ +[" + nick + "] " + key)
-				result = "Results"
-				count = 0
-				for k in jot:
-					if ((key.lower() in k) or (key.lower() in jot[k]['value'].lower())):
-						result = result + "| " + k + " "
-						count+=1
-				if (count > 0):
-					self.bot.privmsg(target, nick + ": " + str(count) + " " + result)
+	def jot_search(self, nick, target, key):
+		with shelve.open('jot.shelf') as jot:
+			result = "Results"
+			count = 0
+			for k in jot:
+				if ((key.lower() in k) or (key.lower() in jot[k]['value'].lower())):
+					result = result + "| " + k + " "
+					count+=1
+			if (count > 0):
+				self.bot.privmsg(target, nick + ": " + str(count) + " " + result)
 		
-	@irc3.event('^(@(?P<tags>\S+) )?:(?P<nick>\S+)(?P<mask>!\S+@\S+) PRIVMSG (?P<target>\S+) :;-(?P<key>[\w\s]+?)\s*$')
-	def jot_remove(self, nick=None, mask=None, target=None, key=None, **kw):
-		if (self.bot.obeying_commands(target) and nick in list(self.bot.channels[target].modes['@'])):
+	def jot_remove(self, nick, target, key):
+		if nick in list(self.bot.channels[target].modes['@']):
 			with shelve.open('jot.shelf') as jot:
 				key = key.lower()
 				if (key in jot):
-					print("JOT~~~ -[" + nick + "] " + jot[key]['key'] + " " + jot[key]['verb'] + " " + jot[key]['value'])
 					del jot[key]
 					self.bot.privmsg(nick, 'Ok.')
