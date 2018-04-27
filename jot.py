a075f5c2a3b6695a0bc70b2fc4eb14e7a994d118
ctburley <calin.burley@gmail.com>
2018-04-27 10:43:01 -0400

    moved things around, added |= multi response saving and made it work

--- jot.py
+++ jot.py
@@ -1,6 +1,7 @@
 import irc3, shelve, re
 from irc3.plugins.command import command
 from irc3.plugins.cron import cron
+from random import choice
 
 @irc3.plugin
 class Plugin:
@@ -11,14 +12,70 @@ class Plugin:
 		self.controlchar = '>'
 		self.features = {
 			'add':	(re.compile('^'+self.controlchar+'(?P<key>[\w\s]+?)\s*=\s*(?P<global>-g)?\s*(?P<data>.*)$'), self.jot_add, ['key', 'data', 'global']),
-			'get':	(re.compile('^'+self.controlchar+'(?P<key>[\w\s]+?)\s*$'), self.jot_get, ['key']),
-			'tell':	(re.compile('^'+self.controlchar+'(?P<key>[\w\s]+?)\s*@\s*(?P<at>\S+)\s*$'), self.jot_get, ['key', 'at']),
+			'also':	(re.compile('^'+self.controlchar+'(?P<key>[\w\s]+?)\s*\|=\s*(?P<global>-g)?\s*(?P<data>.*)$'), self.jot_also, ['key', 'data', 'global']),
+			'get':	(re.compile('^'+self.controlchar+'(?P<key>[\w\s]+?)(?P<global>\s*-g)?\s*$'), self.jot_get, ['key', 'global']),
+			'tell':	(re.compile('^'+self.controlchar+'(?P<key>[\w\s]+?)(?P<global>\s*-g)?\s*@\s*(?P<at>\S+)\s*$'), self.jot_get, ['key', 'global', 'at']),
 			'search':	(re.compile('^'+self.controlchar+'\?(?P<key>[\w\s]+?)\s*$'), self.jot_search, ['key']),
-			'remove':	(re.compile('^'+self.controlchar+'-(?P<key>[\w\s]+?)\s*(?P<global>-g)?$'), self.jot_remove, ['key', 'global'])
-		}
+			'remove':	(re.compile('^'+self.controlchar+'-(?P<key>[\w\s]+?)\s*(?P<global>-g)?$'), self.jot_remove, ['key', 'global'])}
 		self.jot_load()
 		print("JOT ~ LOADED")
 			
+	# --- Features
+	
+	def jot_add(self, nick, target, key, data, globl=None):
+		if globl is not None and nick in list(self.bot.channels[target].modes['@']):
+			target = 'g#l#o#b#a#l'
+		if not self.jot_exists(key, target):
+			self.jot_write(key, {'key':key, 'from':nick, 'value':[data]}, target)
+			self.bot.privmsg(nick, 'Ok.')
+		else:
+			self.bot.privmsg(nick, "The key '"+key+"' already exists.")
+	
+	def jot_also(self, nick, target, key, data, globl=None):
+		if globl is not None and nick in list(self.bot.channels[target].modes['@']):
+			target = 'g#l#o#b#a#l'
+		if self.jot_exists(key, target):
+			result = self.jot_read(key, target)
+			result['value'].append(data)
+			self.jot_write(key, result, target)
+			self.bot.privmsg(nick, 'Ok.')
+		else:
+			self.jot_add(nick, target, key, data, globl)
+			
+	def jot_get(self, nick, target, key, globl=None, at=None):
+		if self.jot_exists(key, target) and globl is None:
+			jot = self.jot_read(key, target)
+			nick = at if at is not None else nick
+			self.bot.privmsg(target, nick + ": " + choice(jot['value']))
+			return
+		if self.jot_exists(key, 'g#l#o#b#a#l'):
+			jot = self.jot_read(key, 'g#l#o#b#a#l')
+			nick = at if at is not None else nick
+			self.bot.privmsg(target, nick + ": " + choice(jot['value']))
+			
+	def jot_search(self, nick, target, key):
+		with shelve.open(self.jotfile) as jot:
+			result = "Results "
+			count = 0
+			if target in self.jots:
+				for k in self.jots[target]:
+					if key.lower() in k:
+						result = result + " " + self.controlchar + k + " "
+						count+=1
+			self.bot.privmsg(target, nick + ": " + str(count) + " " + result)
+		
+	def jot_remove(self, nick, target, key, globl=None):
+		if nick in list(self.bot.channels[target].modes['@']):
+			if globl is not None:
+				target = 'g#l#o#b#a#l'
+			key = key.lower()
+			if target in self.jots:
+				if key in self.jots[target]:
+					del self.jots[target][key]
+					self.bot.privmsg(nick, 'Ok.')
+					
+	# --- Reload & Accessors
+					
 	@classmethod
 	def reload(cls, old):
 		return cls(old.bot)
@@ -43,18 +100,20 @@ class Plugin:
 		if channel in self.jots:
 			if key in self.jots[channel]:
 				return self.jots[channel][key]
-			if key in self.jots['g#l#o#b#a#l']:
-				return self.jots['g#l#o#b#a#l'][key]
 		return None
 	
-	def jot_write(self, key, value, nick, channel='g#l#o#b#a#l'):
+	def jot_write(self, key, value, channel):
 		if channel not in self.jots:
 			self.jots[channel] = {}
-		if key.lower() not in self.jots[channel]:
-			self.jots[channel][key.lower()] = {'key':key, 'from':nick, 'value':value}
-			return True
-		return False
+		result = not self.jot_exists(key, channel)
+		self.jots[channel][key.lower()] = value
+		return result
 	
+	def jot_exists(self, key, channel):
+		return (channel in self.jots) and (key.lower() in self.jots[channel])
+		
+	# ---  Core	
+		
 	@irc3.event('^(@\S+ )?:(?P<nick>\S+)!\S+@\S+ PRIVMSG (?P<target>\S+) :(?P<data>.*)$')
 	def jot_core(self, nick, target, data, **kw):
 		if (self.bot.obeying_commands(target)):
@@ -66,38 +125,4 @@ class Plugin:
 					for arg in args:
 						arglist.append(result.group(arg))
 					func(nick, target, *arglist)
-	
-	def jot_add(self, nick, target, key, data, globl=None):
-		if globl is not None:
-			target = 'g#l#o#b#a#l'
-		if (self.jot_write(key, data, nick, target)):
-			self.bot.privmsg(nick, 'Ok.')
-		else:
-			self.bot.privmsg(nick, "The key '"+key+"' already exists.")
-			
-	def jot_get(self, nick, target, key, at=None):
-		jot = self.jot_read(key, target)
-		if jot:
-			nick = at if at is not None else nick
-			self.bot.privmsg(target, nick + ": " + jot['value'])
-			
-	def jot_search(self, nick, target, key):
-		with shelve.open(self.jotfile) as jot:
-			result = "Results "
-			count = 0
-			if target in self.jots:
-				for k in self.jots[target]:
-					if ((key.lower() in k) or (key.lower() in self.jots[target][k]['value'].lower())):
-						result = result + " " + self.controlchar + k + " "
-						count+=1
-			self.bot.privmsg(target, nick + ": " + str(count) + " " + result)
-		
-	def jot_remove(self, nick, target, key, globl=None):
-		if nick in list(self.bot.channels[target].modes['@']):
-			if globl is not None:
-				target = 'g#l#o#b#a#l'
-			key = key.lower()
-			if target in self.jots:
-				if key in self.jots[target]:
-					del self.jots[target][key]
-					self.bot.privmsg(nick, 'Ok.')
+
