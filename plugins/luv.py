e538222252bde942d5f2060e050db372d8603c27
Calin Burley <calin.burley@gmail.com>
2018-06-17 17:32:27 -0400

    adding !birthday

--- plugins/luv.py
+++ plugins/luv.py
@@ -48,7 +48,25 @@ class UserActions:
             "Holding {target} tight, a content {nick} ponders the evils of a world without snuggling...",
             "{nick} asks {target} whose turn it is to be the big spoon?",
             "{nick} lays their head on {target}'s shoulder and sighs happily.",
-            "Without a care in the world, {nick} snuggle back up with {target}."
+            "Without a care in the world, {nick} snuggles back up with {target}."
+        )
+        
+        self.strings['gifts'] = (
+            "{nick} carefully crafts a gift for {target} out of pipe cleaners and cooked macaroni.",
+            "Lacking the essential pipe cleaners, {nick} cooks a delicious macaroni birthday dinner for {target}.",
+            "{nick} wraps a boxin silver paper and ties it with red string for {target}.",
+            "A box appears before {target}! They open it and are showered with love and money and a note. Opening the note it reads, 'wishing you the best, love {nick}!'.",
+            "{nick} bakes a three tier cake for {target} and sends them flowers at work.",
+            "{nick} sings a birthday song for {target} and dances around wearing a rainbow ghillie suit.",
+            "{nick} hires a singing telegram to rickroll {target} and wish them a happy birthday!",
+            "{nick} orders a pizza and calls {target} over to celebrate!",
+            "{nick} gives {target} a bag from 7-11 full of magazines and packs of gum and a mix-cd of trucker songs.",
+            "{nick} blows the work dust off their project and presents {target} with a life sized replica of Benedict Cumberbatch with the mouth cut out.",
+            "{nick} builds a birthday pie special for {target}.",
+            "{nick} puts another candle on {target}'s cake for good luck.",
+            "{nick} pulls {target} aside to wish them happy birthday privately. ;)",
+            "Jumping out from behind a door {nick} surprises {target} with a gift certificate for a massage.",
+            "{nick} pulls out a wad of cash and hands it to {target} saying, 'We all know what this is for'."
         )
                             
         print("<3 <3 <3 luv ~ LOADE")
@@ -77,4 +95,11 @@ class UserActions:
         """
         target = ' '.join(args['<someone>'])
         self.bot.privmsg(channel, " *** "+str(choice(self.strings['snuggles'])).format(target=target,nick=mask.nick))
-        
+    
+    @command
+    def birthday(self, mask, channel, args):
+        """Celebrate someone's life!
+            %%birthday <someone>...
+        """
+        target = ' '.join(args['<someone>'])
+        self.bot.privmsg(channel, " *** "+str(choice(self.strings['gifts'])).format(target=target,nick=mask.nick))
