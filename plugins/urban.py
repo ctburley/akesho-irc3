097566a3d18576102d1e10410b176ea4ae96bb58
Calin <calin.t.burley@gmail.com>
2018-07-27 16:27:51 -0400

    added random and fixed error on bad idx

--- plugins/urban.py
+++ plugins/urban.py
@@ -13,12 +13,27 @@ class UrbanDict:
     @command(aliases=['u','ud'])
     def urban(self, mask, channel, args):
         """Use urban dictionary to look up words or phrases.
-            %%urban <word>..."""
-        idx = args['<word>'][-1] if args['<word>'][-1].isdigit() else None
-        udict = self.lookup(" ".join(args['<word>'][0:-1] if idx else args['<word>']))
+            %%urban [<word>...]"""
+        if args['<word>']:
+            idx = args['<word>'][-1] if args['<word>'][-1].isdigit() else None
+            udict = self.lookup(" ".join(args['<word>'][0:-1] if idx else args['<word>']))
+        else:
+            (idx, udict) = (None, self.random())
+            
         if udict:
+            idx = idx if idx < len(udict) else None
             self.bot.privmsg(channel, "{word}: {definition}".format(**{'word': udict[int(idx if idx else 0)]['word'], 'definition': udict[int(idx if idx else 0)]['definition'][:400]}))
     
+    def random(self):
+        try:
+            req = urlopen("http://api.urbandictionary.com/v0/random")
+            return loads(req.read().decode())['list']
+        except HTTPError as e:
+            return None
+        except URLError as e:
+            return None
+        return None
+        
     def lookup(self, term):
         try:
             req = urlopen("http://api.urbandictionary.com/v0/define?"+urlencode({'term': term}))
