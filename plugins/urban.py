a253d3e0dfc8fc2d99ca3c43cb6dd68b368fa2b0
Calin <calin.t.burley@gmail.com>
2018-07-27 15:08:39 -0400

    adding urban dict lookup

---
+++ plugins/urban.py
@@ -0,0 +1,31 @@
+from json import loads
+from urllib.parse import urlencode
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
+import irc3
+from irc3.plugins.command import command
+
+@irc3.plugin
+class UrbanDict:
+    def __init__(self, _bot):
+        self.bot = _bot
+        
+    @command(aliases=['u','ud'])
+    def urban(self, mask, channel, args):
+        """Use urban dictionary to look up words or phrases.
+            %%urban <word>..."""
+        idx = args['<word>'][-1] if args['<word>'][-1].isdigit() else None
+        udict = self.lookup(" ".join(args['<word>'][0:-1] if idx else " ".join(args['<word>'])))
+        if udict:
+            self.bot.privmsg(channel, "{word}: {definition}".format(**{'word': udict[int(idx if idx else 0)]['word'], 'definition': udict[int(idx if idx else 0)]['definition'][:400]}))
+    
+    def lookup(self, term):
+        try:
+            req = urlopen("http://api.urbandictionary.com/v0/define?"+urlencode({'term': term}))
+            return loads(req.read().decode())['list']
+        except HTTPError as e:
+            return None
+        except URLError as e:
+            return None
+        return None
+    
