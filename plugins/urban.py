f172440c01cfa42b841794255f73f94909ce3075
Calin <calin.t.burley@gmail.com>
2018-07-27 16:31:44 -0400

    compared none to integer

--- plugins/urban.py
+++ plugins/urban.py
@@ -21,7 +21,7 @@ class UrbanDict:
             (idx, udict) = (None, self.random())
             
         if udict:
-            idx = idx if idx < len(udict) else None
+            idx = idx if idx and idx < len(udict) else None
             self.bot.privmsg(channel, "{word}: {definition}".format(**{'word': udict[int(idx if idx else 0)]['word'], 'definition': udict[int(idx if idx else 0)]['definition'][:400]}))
     
     def random(self):
