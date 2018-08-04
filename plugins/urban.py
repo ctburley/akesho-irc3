e1219514b2147278bbbadad6a21cf036067d4b00
Calin <calin.t.burley@gmail.com>
2018-08-04 10:45:05 -0400

    added linking urban dicts

--- plugins/urban.py
+++ plugins/urban.py
@@ -11,26 +11,38 @@ class UrbanDict:
         self.bot = _bot
         print('urban dict loaded')
         
-    @command(aliases=['u','ud'])
+    @command(aliases=['u','ud'],options_first=True,use_shlex=False)
     def urban(self, mask, channel, args):
         """Use urban dictionary to look up words or phrases.
             %%urban [<word>...]"""
+        (ix, udict, link) = (None, None, False)
         if args['<word>']:
-            ix = int(args['<word>'][-1]) if args['<word>'][-1].isdigit() else None
-            udict = self.lookup(" ".join(args['<word>'][0:-1] if ix else args['<word>']))
+            word = args['<word>']
+            link = False
+            if word[-1].isdigit():
+                ix = int(word[-1])
+                word.pop(-1)
+            if word[0].lower() == '-link' or word[0].lower() == '-l':
+                link = True
+                word.pop(0)
+            udict = self.lookup(" ".join(word))
         else:
-            (ix, udict) = (None, self.lookup())
+            (ix, udict, link) = (None, self.lookup(), False)
             
         if udict:
             idx = ix if ix else 0
             args = {'word': "{}{}: ".format(udict[idx]['word'],(", "+str(idx) if ix else ""))}
-            defi = udict[idx]['definition']
+            if link:
+                defi = udict[idx]['permalink']
+            else:
+                defi = udict[idx]['definition']
             if defi.lower().startswith(udict[idx]['word'].lower()+":"):
                 defi = defi[len(udict[idx]['word'])+1:].strip()
             if (len(defi)+len(args['word'])) > 449:
                 args['definition'] = defi[:445-len(args['word'])] + "[...]"
             else:
                 args['definition'] = defi
+                
             self.bot.privmsg(channel, "{word}{definition}".format(**args))
     
     def lookup(self, term=None):
