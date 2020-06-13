2f8c6167a8993d4c0d084155d6bdf9d4127d89d1
BuildTools <unconfigured@null.spigotmc.org>
2020-06-13 11:10:53 -0400

    oops

--- plugins/yt.py
+++ plugins/yt.py
@@ -3,14 +3,12 @@ from irc3.plugins.command import command
 
 @irc3.plugin
 class Plugin:
+  def __init__(self, bot):
+     = bot
+    print("yt loaded")
 
-	def __init__(self, bot):
-		self.bot = bot
-        print("yt loaded")
-            
-	@irc3.event('^(@(?P<tags>\S+) )?:(?P<nick>\S+)(?P<mask>!\S+@\S+) PRIVMSG (?P<channel>\S+) :\.yt\s+(?P<target>.*?)$')
-	def yt(self, nick=None, mask=None, channel=None, target=None, **kw):
-		if self.bot.obeying_commands(channel):
-			target = target.strip()
-			self.bot.privmsg(channel, "Hey " + nick + " .yt isn't working right now, try '.gse youtube "+target+"' instead! <3")
-				
+  @irc3.event('^(@(?P<tags>\S+) )?:(?P<nick>\S+)(?P<mask>!\S+@\S+) PRIVMSG (?P<channel>\S+) :\.yt\s+(?P<target>.*?)$')
+  def yt(self, nick=None, mask=None, channel=None, target=None, **kw):
+    if self.bot.obeying_commands(channel):
+      target = target.strip()
+      self.bot.privmsg(channel, "Hey " + nick + " .yt isn't working right now, try '.gse youtube "+target+"' instead! <3")
